91c94c6913a0e87d4d5b3b31370bfd8d01a50307
rubailly <rubailly@gmail.com>
2016-02-08 15:38:58 +0200

    Adding PDF generation using XSLFO and FOP

--- omod/src/main/java/org/openmrs/module/mksreports/web/controller/MKSReportsReportsManageController.java
+++ omod/src/main/java/org/openmrs/module/mksreports/web/controller/MKSReportsReportsManageController.java
@@ -13,20 +13,33 @@
  */
 package org.openmrs.module.mksreports.web.controller;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.transform.Result;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.stream.StreamSource;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.fop.apps.FOPException;
+import org.apache.fop.apps.FOUserAgent;
+import org.apache.fop.apps.Fop;
+import org.apache.fop.apps.FopFactory;
+import org.apache.fop.apps.MimeConstants;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.patientsummary.PatientSummaryResult;
 import org.openmrs.module.patientsummary.PatientSummaryTemplate;
 import org.openmrs.module.patientsummary.api.PatientSummaryService;
 import org.openmrs.module.reporting.report.ReportDesign;
 import org.openmrs.module.reporting.report.service.ReportService;
+import org.openmrs.util.OpenmrsClassLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,25 +50,43 @@ import org.springframework.web.bind.annotation.RequestParam;
  * The main controller.
  */
 @Controller
-public class  MKSReportsReportsManageController {
-	
-	protected final Log log = LogFactory.getLog(getClass());
+public class MKSReportsReportsManageController {
 	
 	@RequestMapping(value = "/module/mksreports/manage", method = RequestMethod.GET)
 	public void manage(ModelMap model) {
 		model.addAttribute("user", Context.getAuthenticatedUser());
 	}
 	
+	public Transformer getTransformer(StreamSource streamSource) {
+		// setup the xslt transformer
+		net.sf.saxon.TransformerFactoryImpl impl = new net.sf.saxon.TransformerFactoryImpl();
+		
+		try {
+			return impl.newTransformer(streamSource);
+			
+		}
+		catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 	/**
 	 * Receives requests to run a patient summary.
+	 * 
 	 * @param patientId the id of patient whose summary you wish to view
 	 * @param summaryId the id of the patientsummary you wish to view
 	 */
 	@RequestMapping(value = "/module/mksreports/renderSummary")
 	public void renderSummary(ModelMap model, HttpServletRequest request, HttpServletResponse response,
-							  @RequestParam("patientId") Integer patientId,
-							  @RequestParam(value="download",required=false) boolean download,
-							  @RequestParam(value="print",required=false) boolean print) throws IOException {		
+	                          @RequestParam("patientId") Integer patientId,
+	                          @RequestParam(value = "download", required = false) boolean download,
+	                          @RequestParam(value = "print", required = false) boolean print) throws IOException {
+		System.setProperty("javax.xml.transform.TransformerFactory", "net.sf.saxon.TransformerFactoryImpl");
+		
+		FopFactory fopFactory = FopFactory.newInstance();
+		TransformerFactory tFactory = TransformerFactory.newInstance();
+		
 		try {
 			PatientSummaryService pss = Context.getService(PatientSummaryService.class);
 			
@@ -74,11 +105,59 @@ public class  MKSReportsReportsManageController {
 			PatientSummaryResult result = pss.evaluatePatientSummaryTemplate(ps, patientId, parameters);
 			if (result.getErrorDetails() != null) {
 				result.getErrorDetails().printStackTrace(response.getWriter());
-			} 
-			else {
-				response.setContentType("text/xml");			      
-				response.addHeader("Content-Disposition","attachment; filename=PatientHistory.xml" );
-				response.getOutputStream().write(result.getRawContents());
+			} else {
+
+				// the XML file from which we take the name
+				//StreamSource src = new StreamSource(new ByteArrayInputStream(result.getRawContents()));
+				StreamSource source = new StreamSource(OpenmrsClassLoader.getInstance().getResourceAsStream("sample.xml"));
+				// creation of transform source
+				StreamSource transformSource = new StreamSource(OpenmrsClassLoader.getInstance().getResourceAsStream("sampleStylesheet.xsl"));
+				// create an instance of fop factory
+				// FopFactory fopFactory = FopFactory.newInstance();
+				// a user agent is needed for transformation
+				FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
+				// to store output
+				ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+				
+				Transformer xslfoTransformer;
+				try {
+					xslfoTransformer = getTransformer(transformSource);
+					// Construct fop with desired output format
+					Fop fop;
+					try {
+						fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, outStream);
+						// Resulting SAX events (the generated FO)
+						// must be piped through to FOP
+						Result res = new SAXResult(fop.getDefaultHandler());
+						
+						// Start XSLT transformation and FOP processing
+						try {
+							// everything will happen here..
+							xslfoTransformer.transform(source, res);
+										
+							// to write the content to out put stream
+							byte[] pdfBytes = outStream.toByteArray();
+							response.setContentLength(pdfBytes.length);
+							response.setContentType("application/pdf");
+							response.addHeader("Content-Disposition", "attachment;filename=patientHistory.pdf");
+							response.getOutputStream().write(pdfBytes);
+							response.getOutputStream().flush();
+						}
+						catch (TransformerException e) {
+							throw e;
+						}
+					}
+					catch (FOPException e) {
+						throw e;
+					}
+				}
+				catch (TransformerConfigurationException e) {
+					throw e;
+				}
+				catch (TransformerFactoryConfigurationError e) {
+					throw e;
+				}
+				
 			}
 		}
 		catch (Exception e) {
