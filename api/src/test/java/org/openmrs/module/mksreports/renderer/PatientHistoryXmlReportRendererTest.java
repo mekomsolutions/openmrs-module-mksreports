8512becb227dae916c8a81a8f1f73b886614b6e3
rubailly <rubailly@gmail.com>
2016-02-17 10:42:12 +0200

    MRSCMNS-4: Creating a test patient in the #setup method

--- api/src/test/java/org/openmrs/module/mksreports/renderer/PatientHistoryXmlReportRendererTest.java
+++ api/src/test/java/org/openmrs/module/mksreports/renderer/PatientHistoryXmlReportRendererTest.java
@@ -11,20 +11,26 @@ import java.util.Map;
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
+import org.openmrs.Location;
+import org.openmrs.Patient;
+import org.openmrs.PatientIdentifierType;
 import org.openmrs.Person;
 import org.openmrs.User;
+import org.openmrs.contrib.testdata.TestDataManager;
+import org.openmrs.contrib.testdata.builder.EncounterBuilder;
 import org.openmrs.module.mksreports.dataset.definition.PatientHistoryEncounterAndObsDataSetDefinition;
 import org.openmrs.module.reporting.common.SortCriteria;
 import org.openmrs.module.reporting.data.patient.library.BuiltInPatientDataLibrary;
 import org.openmrs.module.reporting.dataset.definition.PatientDataSetDefinition;
 import org.openmrs.module.reporting.report.ReportData;
 import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.converters.reflection.Sun14ReflectionProvider;
 
-public class PatientHistoryXmlReportRendererTest {
+public class PatientHistoryXmlReportRendererTest extends BaseModuleContextSensitiveTest {
 	
 	private static String OUTPUT_XML_OUTPUT_DIR = "target/test/";
 	
@@ -33,14 +39,28 @@ public class PatientHistoryXmlReportRendererTest {
 	@Autowired
 	private BuiltInPatientDataLibrary builtInPatientData;
 	
+	@Autowired
+	private TestDataManager data;
+	
 	private PatientHistoryXmlReportRenderer renderer = new PatientHistoryXmlReportRenderer();
 	
 	private ReportData reportData = null;
 	
 	private File file = null;
 	
+	private Patient p1 =null;
+	
 	@Before
-	public void setUp() throws IOException {
+	public void setUp() throws Exception {
+		
+		executeDataSet("org/openmrs/module/mksreports/include/ReportTestDataset.xml");
+		
+		PatientIdentifierType testIdentifierType = data.getPatientService().getPatientIdentifierType(4);
+		Location testLocation = data.getLocationService().getLocation(1);
+		
+		p1 = data.patient().name("Alice", "MKS Test").gender("F").birthdate("1975-01-02", false).dateCreated("2013-10-01").identifier(testIdentifierType, "Y2ATDN", testLocation).save();
+		
+		
 		InputStream inStream = getClass().getClassLoader().getResourceAsStream("sampleReportData.xml");
 		String str = IOUtils.toString(inStream, "UTF-8");
 		XStream xstream = new XStream(new Sun14ReflectionProvider());
