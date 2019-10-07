0877e1f863512241acef8852a072536b52d87e60
Guimino Neves <minoneves@gmail.com>
2019-10-07 23:36:12 +0200

    added more unit test methods

--- api/src/test/java/org/openmrs/module/mksreports/reports/PatientHistoryManagerTest.java
+++ api/src/test/java/org/openmrs/module/mksreports/reports/PatientHistoryManagerTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
@@ -12,6 +13,7 @@ import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.hamcrest.collection.IsCollectionWithSize;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openmrs.Cohort;
 import org.openmrs.module.mksreports.MKSReportManager;
@@ -40,6 +42,15 @@ public class PatientHistoryManagerTest extends BaseReportTest {
 	@Autowired
 	@Qualifier(MKSReportsConstants.COMPONENT_REPORTMANAGER_PATIENTHISTORY)
 	private MKSReportManager reportManager;
+
+	private static final String XML_DATASET_PATH = "org/openmrs/module/mksreports/include/";
+
+	private static final String XML_REPORT_TEST_DATASET = "patientHistoryManagerTestDataset.xml";
+
+	@Before
+	public void setUp() throws Exception {
+		executeDataSet(XML_DATASET_PATH + XML_REPORT_TEST_DATASET);
+	}
 
 	@Test
 	public void setupReport_shouldSetupPatientHistory() throws Exception {
@@ -73,10 +84,9 @@ public class PatientHistoryManagerTest extends BaseReportTest {
 		EvaluationContext context = new EvaluationContext();
 		context.setBaseCohort(new Cohort("7"));
 		ReportDefinition rd = this.reportManager.constructReportDefinition();
-
 		ReportData reportData = this.reportDefinitionService.evaluate(rd, context);
 		assertNotNull(reportData.getDataSets());
-		DataSet dataSet = reportData.getDataSets().get("demographics");
+		DataSet dataSet = reportData.getDataSets().get(PatientHistoryReportManager.DATASET_KEY_DEMOGRAPHICS);
 		assertNotNull(dataSet);
 		assertNotNull(dataSet.getMetaData());
 		assertNotNull(dataSet.getMetaData().getColumns());
@@ -100,6 +110,80 @@ public class PatientHistoryManagerTest extends BaseReportTest {
 		assertEquals("43", getStringValue(dataSetRow, "Current Age"));
 		assertEquals("F", getStringValue(dataSetRow, "Gender"));
 		assertEquals("Kapina", getStringValue(dataSetRow, "Address"));
+	}
+
+	@Test
+	public void evaluate_shouldReturnObsVitals() throws Exception {
+
+		EvaluationContext context = new EvaluationContext();
+		context.setBaseCohort(new Cohort("2"));
+		ReportDefinition rd = this.reportManager.constructReportDefinition();
+		ReportData reportData = this.reportDefinitionService.evaluate(rd, context);
+
+		assertNotNull(reportData.getDataSets());
+		DataSet dataSet = reportData.getDataSets().get(PatientHistoryReportManager.DATASET_KEY_OBS);
+		assertNotNull(dataSet);
+		assertNotNull(dataSet.getMetaData());
+		assertNotNull(dataSet.getMetaData().getColumns());
+		MatcherAssert.assertThat(dataSet.getMetaData().getColumns(), Matchers.hasSize(6));
+
+		@SuppressWarnings("unchecked")
+		Matcher<Iterable<? extends Object>> containsInAnyOrder = Matchers.containsInAnyOrder(
+				Matchers.hasProperty("name", is(PatientHistoryReportManager.ENCOUNTER_UUID_LABEL)),
+				Matchers.hasProperty("name", is(PatientHistoryReportManager.ENCOUNTER_PROVIDER_LABEL)),
+				Matchers.hasProperty("name", is(PatientHistoryReportManager.OBS_DATETIME_LABEL)),
+				Matchers.hasProperty("name", is(PatientHistoryReportManager.OBS_DATATYPE_LABEL)),
+				Matchers.hasProperty("name", is(PatientHistoryReportManager.OBS_NAME_LABEL)),
+				Matchers.hasProperty("name", is(PatientHistoryReportManager.OBS_VALUE_LABEL)));
+		MatcherAssert.assertThat(dataSet.getMetaData().getColumns(), containsInAnyOrder);
+
+		List<DataSetRow> allDataSetRow = new ArrayList<DataSetRow>();
+		dataSet.forEach(d -> allDataSetRow.add(d));
+
+		DataSetRow HeightDataSetRow = allDataSetRow.get(0);
+		assertEquals("Height (cm)", getStringValue(HeightDataSetRow, PatientHistoryReportManager.OBS_NAME_LABEL));
+		assertEquals("180.0", getStringValue(HeightDataSetRow, PatientHistoryReportManager.OBS_VALUE_LABEL));
+		assertEquals("53acc256-a393-42f2-a32c-b45dae4efd25",
+				getStringValue(HeightDataSetRow, PatientHistoryReportManager.ENCOUNTER_UUID_LABEL));
+		assertEquals("Super User",
+				getStringValue(HeightDataSetRow, PatientHistoryReportManager.ENCOUNTER_PROVIDER_LABEL));
+		assertEquals("2019-01-01 00:00:00.0",
+				getStringValue(HeightDataSetRow, PatientHistoryReportManager.OBS_DATETIME_LABEL));
+		assertEquals("Numeric", getStringValue(HeightDataSetRow, PatientHistoryReportManager.OBS_DATATYPE_LABEL));
+
+		DataSetRow weightDataSetRow = allDataSetRow.get(1);
+		assertEquals("WEIGHT (KG)", getStringValue(weightDataSetRow, PatientHistoryReportManager.OBS_NAME_LABEL));
+		assertEquals("100.0", getStringValue(weightDataSetRow, PatientHistoryReportManager.OBS_VALUE_LABEL));
+		assertEquals("53acc256-a393-42f2-a32c-b45dae4efd25",
+				getStringValue(weightDataSetRow, PatientHistoryReportManager.ENCOUNTER_UUID_LABEL));
+		assertEquals("Super User",
+				getStringValue(weightDataSetRow, PatientHistoryReportManager.ENCOUNTER_PROVIDER_LABEL));
+		assertEquals("2019-01-01 00:00:00.0",
+				getStringValue(weightDataSetRow, PatientHistoryReportManager.OBS_DATETIME_LABEL));
+		assertEquals("Numeric", getStringValue(weightDataSetRow, PatientHistoryReportManager.OBS_DATATYPE_LABEL));
+
+		DataSetRow temperatureDataSetRow = allDataSetRow.get(2);
+		assertEquals("Temperature (C)",
+				getStringValue(temperatureDataSetRow, PatientHistoryReportManager.OBS_NAME_LABEL));
+		assertEquals("34.0", getStringValue(temperatureDataSetRow, PatientHistoryReportManager.OBS_VALUE_LABEL));
+		assertEquals("53acc256-a393-42f2-a32c-b45dae4efd25",
+				getStringValue(temperatureDataSetRow, PatientHistoryReportManager.ENCOUNTER_UUID_LABEL));
+		assertEquals("Super User",
+				getStringValue(temperatureDataSetRow, PatientHistoryReportManager.ENCOUNTER_PROVIDER_LABEL));
+		assertEquals("2019-01-01 00:00:00.0",
+				getStringValue(temperatureDataSetRow, PatientHistoryReportManager.OBS_DATETIME_LABEL));
+		assertEquals("Numeric", getStringValue(temperatureDataSetRow, PatientHistoryReportManager.OBS_DATATYPE_LABEL));
+
+		DataSetRow pulseDataSetRow = allDataSetRow.get(3);
+		assertEquals("Pulse", getStringValue(pulseDataSetRow, PatientHistoryReportManager.OBS_NAME_LABEL));
+		assertEquals("90.0", getStringValue(pulseDataSetRow, PatientHistoryReportManager.OBS_VALUE_LABEL));
+		assertEquals("53acc256-a393-42f2-a32c-b45dae4efd25",
+				getStringValue(pulseDataSetRow, PatientHistoryReportManager.ENCOUNTER_UUID_LABEL));
+		assertEquals("Super User",
+				getStringValue(pulseDataSetRow, PatientHistoryReportManager.ENCOUNTER_PROVIDER_LABEL));
+		assertEquals("2019-01-01 00:00:00.0",
+				getStringValue(pulseDataSetRow, PatientHistoryReportManager.OBS_DATETIME_LABEL));
+		assertEquals("Numeric", getStringValue(pulseDataSetRow, PatientHistoryReportManager.OBS_DATATYPE_LABEL));
 	}
 
 	private String getStringValue(DataSetRow row, String columnName) {
